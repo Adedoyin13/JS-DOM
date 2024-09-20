8fc6af9034549fd18db02cb0b8998560433c7f09
Adedoyin13 <olajideadedoyinrodiyat@gmail.com>
2024-09-20 23:35:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// CreateElement
+// removeChild
+// appendChild
+// replaceChild
+// write
+
+// SELECTORS ==>
+
+document.getElementById
+document.getElementsByClassName
+document.getElementsByName
+document.getElementsByTagName
+
+document.querySelector
+document.querySelectorAll
+
+
+
+
+document.getElementById("demo").innerHTML = 'Hello World!'
+
+const bush = document.getElementsByClassName('mean')
+
+Array.from(bush).forEach((element) => {
+    element.innerHTML = 'bush'
+})
+
+
+
